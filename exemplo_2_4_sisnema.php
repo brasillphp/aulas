4db259f621754d6ac51c7caf3e05a59a87ecd711
Brasill PHP <brasillphp@gmail.com>
2015-08-20 20:11:41 -0300

    novos arquivos aula200815

--- exemplo_2_4_sisnema.php
+++ exemplo_2_4_sisnema.php
@@ -3,7 +3,7 @@
 	
 	$bd = new mysql('localhost', 'root', '', 'cake');
 	
-	$insert = $bd->insert('articles', array('title', 'body'), array('aluno 20', 'novo nome'));	
+	$insert = $bd->insert('articles', array('title', 'body'), array('aluno 120', 'novo nome'), array('aluno 220', 'novo nome'), array('aluno 320', 'novo nome'), array('aluno 420', 'novo nome'));	
 	
 	if($insert)
 	{
