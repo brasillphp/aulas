7e23ce950cc865039513fe867c4bb615a2725e08
Brasill PHP <brasillphp@gmail.com>
2015-08-20 22:29:24 -0300

    novos de novo

---
+++ exemplo_2_6.php
@@ -0,0 +1,8 @@
+<?php
+	require_once('classes/classe_bancodados.inc');
+	
+	$bd = new mysql('localhost', 'root', '', 'cake');
+	
+	$bd->find('articles', 'title', 'aluno',  true);
+	
+	var_dump($bd->navega($_GET['posicao']));
